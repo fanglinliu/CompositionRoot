5285029a4293c093104b66109891681d34b4a327
Fanglin Liu <fanglliu@cisco.com>
2017-08-22 14:33:42 +0800

    Initial Commit

---
+++ CompositionRoot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CompositionRoot
+//
+//  Created by Fanglin Liu on 22/08/2017.
+//  Copyright © 2017 cmbu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
