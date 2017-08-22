12de539cc000ed8f09e9a4b948bd698795313ba3
Fanglin Liu <fanglliu@cisco.com>
2017-08-22 15:50:21 +0800

    DI using ApplicationContext

--- CompositionRoot/D.swift
+++ CompositionRoot/D.swift
@@ -3,52 +3,20 @@ protocol DProtocol {
 }
 
 class D: DProtocol {
-    let f: FProtocol
-    let g: GProtocol
-    let h: HProtocol
-    let i: IProtocol
-    let j: JProtocol
-    let k: KProtocol
-    let l: LProtocol
-    let m: MProtocol
-    let n: NProtocol
-    let o: OProtocol
-    let p: PProtocol
-    
-    init(f: FProtocol,
-         g: GProtocol,
-         h: HProtocol,
-         i: IProtocol,
-         j: JProtocol,
-         k: KProtocol,
-         l: LProtocol,
-         m: MProtocol,
-         n: NProtocol,
-         o: OProtocol,
-         p: PProtocol
-         ) {
-        self.f = f
-        self.g = g
-        self.h = h
-        self.i = i
-        self.j = j
-        self.k = k
-        self.l = l
-        self.m = m
-        self.n = n
-        self.o = o
-        self.p = p
+    private let applicationContext: ApplicationContext
+    init(applicationContext: ApplicationContext) {
+        self.applicationContext = applicationContext
     }
     
     func dMethod() {
-        f.fMethod1()
-        g.gMethod1()
-        h.hMethod()
-        i.iMethod()
-        j.jMethod()
-        k.kMethod()
-        l.lMethod()
-        m.mMethod()
-        n.nMethod()
+        applicationContext.f.fMethod1()
+        applicationContext.g.gMethod1()
+        applicationContext.h.hMethod()
+        applicationContext.i.iMethod()
+        applicationContext.j.jMethod()
+        applicationContext.k.kMethod()
+        applicationContext.l.lMethod()
+        applicationContext.m.mMethod()
+        applicationContext.n.nMethod()
     }
 }
